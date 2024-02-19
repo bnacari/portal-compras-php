8b4ff59af33647f8aaa9193fcfaa88937c29283e
bruno.nacari <bruno.nacari@cesan.com.br>
2024-02-19 09:25:37 -0300

    dropdown usando select2

--- html/cadItemMenu.php
+++ html/cadItemMenu.php
@@ -56,3 +56,32 @@ include('protectAdmin.php');
         </div>
     </fieldset>
 </div>
+
+<!-- Inclua o jQuery Mask Plugin -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> -->
+
+<!-- JS for jQuery -->
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+<!-- CSS for searching -->
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<!-- JS for searching -->
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+
+
+<script>
+    function exibirAlertaIdMenu() {
+        alert("Por favor, preencha o campo 'Menu relacionado'");
+    }
+
+    $(document).ready(function() {
+        $('#idSubMenu').select2({
+            width: '100%',
+            // placeholder: 'Selecione as partes do corpo',
+            allowClear: true
+        });
+
+
+    });
+</script>
--- html/cadSubMenu.php
+++ html/cadSubMenu.php
@@ -22,8 +22,8 @@ include('protectAdmin.php');
             </div>
 
             <div class="input-field col s6">
-                <select name="idMenu" id="idMenu" required>
-                    <option value='' disabled>Selecione uma opção</option>
+                <select name="idMenu" id="idMenu" required oninvalid="exibirAlertaIdMenu()">
+                    <option value='' disabled selected>Selecione uma opção</option>
                     <?php
                     $querySelect2 = "SELECT * FROM [portalcompras].[dbo].[menu] WHERE DT_EXC_MENU IS NULL ORDER BY NM_MENU";
                     $querySelect = $pdoCAT->query($querySelect2);
@@ -55,3 +55,33 @@ include('protectAdmin.php');
         </div>
     </fieldset>
 </div>
+
+
+<!-- Inclua o jQuery Mask Plugin -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> -->
+
+<!-- JS for jQuery -->
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+<!-- CSS for searching -->
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<!-- JS for searching -->
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+
+
+<script>
+    function exibirAlertaIdMenu() {
+        alert("Por favor, preencha o campo 'Menu relacionado'");
+    }
+
+    $(document).ready(function() {
+        $('#idMenu').select2({
+            width: '100%',
+            // placeholder: 'Selecione as partes do corpo',
+            allowClear: true
+        });
+
+
+    });
+</script>
